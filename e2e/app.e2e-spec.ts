3e12c1f99ae605356c503680865ca6c33cb7c02f
Lane Olson <lane@onlea.org>
2017-08-18 13:30:42 -0600

    fix e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('eviction-maps App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should have the map element', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getMapElement().isPresent()).toBeTruthy();
   });
 });
