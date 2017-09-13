63a312abb5806c3dd86164621fa2211ce03690d1
pjsier <pjsier@gmail.com>
2017-09-13 16:18:34 -0500

    Fix search tests

--- src/app/map-ui/predictive-search/predictive-search.component.spec.ts
+++ src/app/map-ui/predictive-search/predictive-search.component.spec.ts
@@ -10,7 +10,6 @@ describe('PredictiveSearchComponent', () => {
   let inputEl;
   const defaultOptionsLimit = 5;
   const defaultOptionField = 'name';
-  // const keyEvent = new KeyboardEvent('keyup', {target: {value: 't'}});
   const defaultOptions = [
     {name: 'Test 1'},
     {name: 'Test 2'},
@@ -42,23 +41,37 @@ describe('PredictiveSearchComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display results on input', fakeAsync(() => {
-    inputEl.nativeElement.value = 't';
-    inputEl.triggerEventHandler('keyup', null);
-    tick();
+  it('should display results on input', async(() => {
+    const el = inputEl.nativeElement;
+    el.value = 't';
+    const event = new KeyboardEvent('keyup', { 'key': 't' });
+    el.dispatchEvent(event);
     fixture.detectChanges();
-    expect(fixture.debugElement.query(By.css('ul')) == null).toBeFalsy();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('.dropdown') == null).toBeFalsy();
+    });
   }));
 
-  it('should display the option field property', fakeAsync(() => {
-    inputEl.nativeElement.value = 't';
-    inputEl.triggerEventHandler('keyup', null);
-    tick();
+  it('should display the option field property', async(() => {
+    const el = inputEl.nativeElement;
+    el.value = 't';
+    const event = new KeyboardEvent('keyup', { 'key': 't' });
+    el.dispatchEvent(event);
     fixture.detectChanges();
-    const firstResult = fixture.debugElement.query(By.css('li'));
-    expect(firstResult.nativeElement.textContent).toContain(defaultOptions[0].name);
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('li').textContent)
+        .toContain(defaultOptions[0].name);
+    });
   }));
 
   it('should emit the selection when clicked', fakeAsync(() => {
+    let selection;
+    component.selectionChange.subscribe((sel) => { selection = sel; });
+    component.updateSelection(defaultOptions[0]);
+    tick();
+    fixture.detectChanges();
+    expect(selection).toEqual(defaultOptions[0]);
   }));
 });
