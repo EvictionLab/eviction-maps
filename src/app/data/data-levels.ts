a28855b88bec8db234e8cb155c2a8962b5af0c5c
pjsier <pjsier@gmail.com>
2017-10-09 11:09:07 -0500

    Re-order data levels, set minzoom

--- src/app/data/data-levels.ts
+++ src/app/data/data-levels.ts
@@ -2,28 +2,51 @@ import { MapLayerGroup } from '../map/map-layer-group';
 
 export const DataLevels: Array<MapLayerGroup> = [
     {
-       'id': 'blockgroups',
-       'name': 'Block Groups',
-       'layerIds': [
-          'blockgroups',
-          'blockgroups_stroke',
-          'blockgroups_bubbles',
-          'blockgroups_text'
-       ],
-       'minzoom': 7,
-       'zoom': [ 10, 16 ]
+        'id': 'states',
+        'name': 'States',
+        'layerIds': [
+            'states',
+            'states_stroke',
+            'states_bubbles',
+            'states_text'
+        ],
+        'minzoom': 0,
+        'zoom': [0, 5]
     },
     {
-       'id': 'zipcodes',
-       'name': 'Zip Codes',
-       'layerIds': [
-        'zipcodes',
-        'zipcodes_stroke',
-        'zipcodes_bubbles',
-        'zipcodes_text'
-       ],
-       'minzoom': 6,
-       'zoom': [ 9, 10 ]
+        'id': 'counties',
+        'name': 'Counties',
+        'layerIds': [
+            'counties',
+            'counties_stroke',
+            'counties_bubbles',
+            'counties_text'
+        ],
+        'minzoom': 0,
+        'zoom': [5, 8]
+    },
+    {
+        'id': 'zipcodes',
+        'name': 'Zip Codes',
+        'layerIds': [
+            'zipcodes',
+            'zipcodes_stroke',
+            'zipcodes_bubbles',
+            'zipcodes_text'
+        ],
+        'minzoom': 5,
+        'zoom': [9, 10]
+    },
+    {
+        'id': 'cities',
+        'name': ' Cities',
+        'layerIds': [
+            'cities',
+            'cities_stroke',
+            'cities_bubbles',
+            'cities_text'
+        ],
+        'minzoom': 5,
     },
     {
        'id': 'tracts',
@@ -38,38 +61,15 @@ export const DataLevels: Array<MapLayerGroup> = [
        'zoom': [ 8, 9 ]
     },
     {
-       'id': 'cities',
-       'name': ' Cities',
-       'layerIds': [
-          'cities',
-          'cities_stroke',
-          'cities_bubbles',
-          'cities_text'
-       ],
-       'minzoom': 0,
-    },
-    {
-       'id': 'counties',
-       'name': 'Counties',
-       'layerIds': [
-          'counties',
-          'counties_stroke',
-          'counties_bubbles',
-          'counties_text'
-       ],
-       'minzoom': 0,
-       'zoom': [ 5, 8 ]
-    },
-    {
-       'id': 'states',
-       'name': 'States',
-       'layerIds': [
-          'states',
-          'states_stroke',
-          'states_bubbles',
-          'states_text'
-       ],
-       'minzoom': 0,
-       'zoom': [ 0, 5 ]
+        'id': 'blockgroups',
+        'name': 'Block Groups',
+        'layerIds': [
+            'blockgroups',
+            'blockgroups_stroke',
+            'blockgroups_bubbles',
+            'blockgroups_text'
+        ],
+        'minzoom': 7,
+        'zoom': [10, 16]
     }
  ];
