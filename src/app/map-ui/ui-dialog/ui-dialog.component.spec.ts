5deb5c6554c3d3d53987be6ff676e51d793c84f0
Lane Olson <lane@onlea.org>
2017-10-11 13:53:11 -0600

    add tests for non-bootstrap behaviours

--- src/app/map-ui/ui-dialog/ui-dialog.component.spec.ts
+++ src/app/map-ui/ui-dialog/ui-dialog.component.spec.ts
@@ -1,4 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, tick, fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ModalModule, BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { FormsModule } from '@angular/forms';
 
@@ -20,10 +21,26 @@ describe('UiDialogComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UiDialogComponent);
     component = fixture.componentInstance;
+    component.title = 'test title';
+    component.content = [
+      { type: 'text', data: 'hello world' },
+      { type: 'checkbox', data: { value: false, label: 'check test' } }
+    ];
+    component.buttons = { ok: true, cancel: true };
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close dialog and emit data on button click', fakeAsync(() => {
+    let returnValue;
+    component.buttonClicked.subscribe((d) => { returnValue = d; });
+    const buttons = fixture.debugElement.queryAll(By.css('.btn'));
+    buttons[0].triggerEventHandler('click', null);
+    tick();
+    fixture.detectChanges();
+    expect(returnValue.accepted).toBeTruthy();
+  }));
 });
