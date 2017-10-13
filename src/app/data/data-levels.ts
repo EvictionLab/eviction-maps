ca2fbc636107f4a77e988a46e51f009e19183609
pjsier <pjsier@gmail.com>
2017-10-13 13:18:57 -0500

    Fix auto switch layers by re-ordering data levels
    Not sure why this hadn't been triggered earlier (potentially
    refactoring), but the initial on-change event for the UI-select was
    getting triggered after render, which called setGroupVisibility and
    disabled auto-switching. We should probably have the layers in
    descending order anyway, so I updated that and it works

--- src/app/data/data-levels.ts
+++ src/app/data/data-levels.ts
@@ -2,6 +2,60 @@ import { MapLayerGroup } from '../map/map-layer-group';
 
 export const DataLevels: Array<MapLayerGroup> = [
     {
+        'id': 'states',
+        'name': 'States',
+        'layerIds': [
+            'states',
+            'states_stroke',
+            'states_bubbles',
+            'states_text'
+        ],
+        'zoom': [0, 5]
+    },
+    {
+        'id': 'counties',
+        'name': 'Counties',
+        'layerIds': [
+            'counties',
+            'counties_stroke',
+            'counties_bubbles',
+            'counties_text'
+        ],
+        'zoom': [5, 8]
+    },
+    {
+        'id': 'zipcodes',
+        'name': 'Zip Codes',
+        'layerIds': [
+            'zipcodes',
+            'zipcodes_stroke',
+            'zipcodes_bubbles',
+            'zipcodes_text'
+        ],
+        'zoom': [9, 10]
+    },
+    {
+        'id': 'cities',
+        'name': ' Cities',
+        'layerIds': [
+            'cities',
+            'cities_stroke',
+            'cities_bubbles',
+            'cities_text'
+        ]
+    },
+    {
+        'id': 'tracts',
+        'name': 'Tracts',
+        'layerIds': [
+            'tracts',
+            'tracts_stroke',
+            'tracts_bubbles',
+            'tracts_text'
+        ],
+        'zoom': [8, 9]
+    },
+    {
        'id': 'blockgroups',
        'name': 'Block Groups',
        'layerIds': [
@@ -11,59 +65,5 @@ export const DataLevels: Array<MapLayerGroup> = [
           'blockgroups_text'
        ],
        'zoom': [ 10, 16 ]
-    },
-    {
-       'id': 'zipcodes',
-       'name': 'Zip Codes',
-       'layerIds': [
-        'zipcodes',
-        'zipcodes_stroke',
-        'zipcodes_bubbles',
-        'zipcodes_text'
-       ],
-       'zoom': [ 9, 10 ]
-    },
-    {
-       'id': 'tracts',
-       'name': 'Tracts',
-       'layerIds': [
-          'tracts',
-          'tracts_stroke',
-          'tracts_bubbles',
-          'tracts_text'
-       ],
-       'zoom': [ 8, 9 ]
-    },
-    {
-       'id': 'cities',
-       'name': ' Cities',
-       'layerIds': [
-          'cities',
-          'cities_stroke',
-          'cities_bubbles',
-          'cities_text'
-       ]
-    },
-    {
-       'id': 'counties',
-       'name': 'Counties',
-       'layerIds': [
-          'counties',
-          'counties_stroke',
-          'counties_bubbles',
-          'counties_text'
-       ],
-       'zoom': [ 5, 8 ]
-    },
-    {
-       'id': 'states',
-       'name': 'States',
-       'layerIds': [
-          'states',
-          'states_stroke',
-          'states_bubbles',
-          'states_text'
-       ],
-       'zoom': [ 0, 5 ]
     }
  ];
