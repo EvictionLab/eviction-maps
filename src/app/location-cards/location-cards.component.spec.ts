0dca551279167fa756fb8bb38e18a1516459e17f
Lane Olson <lane@onlea.org>
2017-10-27 11:44:55 -0600

    add search service stub to tests

--- src/app/location-cards/location-cards.component.spec.ts
+++ src/app/location-cards/location-cards.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { TypeaheadModule, TypeaheadMatch } from 'ngx-bootstrap/typeahead';
 import { LocationCardsComponent } from './location-cards.component';
+import { PredictiveSearchComponent } from '../map-ui/predictive-search/predictive-search.component';
+import { SearchService } from '../search/search.service';
 
 describe('LocationCardsComponent', () => {
   let component: LocationCardsComponent;
   let fixture: ComponentFixture<LocationCardsComponent>;
+  const searchServiceStub = {
+    queryGeocoder: () => {},
+    getLayerName: () => {},
+    getTileData: () => {},
+    query: ''
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationCardsComponent ]
+      declarations: [ LocationCardsComponent, PredictiveSearchComponent ],
+      imports: [ FormsModule, TypeaheadModule.forRoot() ],
+      providers: [ {provide: SearchService, useValue: searchServiceStub } ]
     })
     .compileComponents();
   }));
