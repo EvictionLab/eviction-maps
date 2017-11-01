0e54da0d2f4f7f907da2bc6fdd131b126cb24bef
Lane Olson <lane@onlea.org>
2017-11-01 16:54:00 -0600

    remove data querying from search service

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,14 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Http, Response, ResponseContentType } from '@angular/http';
-import { MapFeature } from '../map/map-feature';
-import * as SphericalMercator from '@mapbox/sphericalmercator';
-import * as vt from '@mapbox/vector-tile';
-import * as Protobuf from 'pbf';
-import * as inside from '@turf/inside';
-import * as bbox from '@turf/bbox';
-import 'rxjs/add/observable/forkJoin';
 
 const MapzenLayerMap = {
   'region': 'states',
@@ -20,7 +12,6 @@ const MapzenLayerMap = {
 
 @Injectable()
 export class SearchService {
-  tilesetYears = ['90', '00', '10'];
   apiKey = 'mapzen-FgUaZ97';
   mapzenParams = [
     'sources=whosonfirst,openstreetmap',
@@ -29,11 +20,7 @@ export class SearchService {
     'api_key=' + this.apiKey
   ];
   mapzenBase = 'https://search.mapzen.com/v1/autocomplete?';
-  tileBase = 'https://s3.us-east-2.amazonaws.com/eviction-lab-tilesets/fixtures/';
-  maxZoom = 10;
-  mercator = new SphericalMercator({ size: 256 });
   query: string;
-  private _results = new BehaviorSubject<Object[]>([]);
   results: Observable<Object[]>;
 
   constructor(private http: Http) {
@@ -60,64 +47,6 @@ export class SearchService {
    */
   getLayerName(layerStr: string) {
     return MapzenLayerMap.hasOwnProperty(layerStr) ? MapzenLayerMap[layerStr] : 'block-groups';
-  }
-
-  /**
-   * Takes the layer to be queried and coordinates for an object,
-   * determines which tile to request, parses it, and then returns
-   * the first feature that the includes the coordinate point
-   *
-   * @param layerId ID of layer to query for tile data
-   * @param lonLat array of [lon, lat]
-   * @param multiYear specifies whether to merge multiple year tiles
-   */
-  getTileData(layerId: string, lonLat: number[], multiYear = false): Observable<MapFeature> {
-    const point = {
-      type: 'Feature',
-      geometry: { type: 'Point', coordinates: lonLat },
-      properties: {}
-    } as GeoJSON.Feature<GeoJSON.Point>;
-    const xyzCoords = this.mercator.xyz([lonLat[0], lonLat[1], lonLat[0], lonLat[1]], 10);
-    const coords = { x: xyzCoords.maxX, y: xyzCoords.maxY };
-
-    // Defining function block to retain scope
-    const parseTile = (res: Response): MapFeature => {
-      const tile = new vt.VectorTile(new Protobuf(res.arrayBuffer()));
-      const layer = tile.layers[layerId];
-      for (let i = 0; i < layer.length; ++i) {
-        const feat = layer.feature(i).toGeoJSON(coords.x, coords.y, this.maxZoom);
-        if (inside(point, feat)) {
-          feat.properties.bbox = bbox(feat);
-          return feat;
-        }
-      }
-      // Return empty object if nothing found for now
-      return {} as MapFeature;
-    };
-
-    if (multiYear) {
-      const tileRequests = this.tilesetYears.map((y) => {
-        return this.http.get(
-          `${this.tileBase}evictions-${layerId}-${y}/` +
-          `${this.maxZoom}/${coords['x']}/${coords['y']}.pbf`,
-          { responseType: ResponseContentType.ArrayBuffer }
-        ).map(parseTile);
-      });
-
-      return Observable.forkJoin(tileRequests)
-        .map((features: any[]) => {
-          const feat = features[0];
-          for (let i = 1; i < this.tilesetYears.length; ++i) {
-            feat['properties'] = { ...feat['properties'], ...features[i]['properties']};
-          }
-          return feat as MapFeature;
-        });
-    } else {
-      return this.http.get(
-        `${this.tileBase}evictions-${layerId}/${this.maxZoom}/${coords.x}/${coords.y}.pbf`,
-        { responseType: ResponseContentType.ArrayBuffer }
-      ).map(parseTile);
-    }
   }
 
 }
