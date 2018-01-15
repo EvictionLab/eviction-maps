5a6d23f6c0e95c5706521cc1a59e8661067f1db2
Lane <laneolson@gmail.com>
2018-01-15 16:41:32 -0700

    fix crashing tests

--- src/app/ui/progress-bar/progress-bar.component.spec.ts
+++ src/app/ui/progress-bar/progress-bar.component.spec.ts
@@ -22,4 +22,24 @@ describe('ProgressBarComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be visible when indeterminate is set', () => {
+    component.indeterminate = true;
+    fixture.detectChanges();
+    expect(fixture.debugElement.styles.display)
+      .toBe('block');
+  });
+
+  it('should be visible when progress is set', () => {
+    component.progress = 50;
+    fixture.detectChanges();
+    expect(fixture.debugElement.styles.display)
+      .toBe('block');
+  });
+
+  it('should be hidden when neither progress or indeterminate is set', () => {
+    expect(fixture.debugElement.styles.display)
+      .toBe('none');
+  });
+
 });
