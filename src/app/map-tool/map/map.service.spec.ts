b73d56be1135fe2951cc4efddcc57b08e9130e89
pjsier <pjsier@gmail.com>
2018-01-23 14:08:25 -0600

    Tests on map service

--- src/app/map-tool/map/map.service.spec.ts
+++ src/app/map-tool/map/map.service.spec.ts
@@ -12,4 +12,34 @@ describe('MapService', () => {
   it('should be created', inject([MapService], (service: MapService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should return null from getUnionFeature with an empty array',
+    inject([MapService], (service: MapService) => {
+      const featureStub = {
+        type: 'Feature',
+        properties: { 'n': 'name' },
+        geometry: { type: 'Polygon', coordinates: [] }
+      } as GeoJSON.Feature<GeoJSON.Polygon>;
+      const mapboxStub = {
+        queryRenderedFeatures: (a, b) => []
+      };
+      service.setMapInstance(mapboxStub);
+      expect(service.getUnionFeature('layer', featureStub)).toEqual(null);
+    }
+  ));
+
+  it('should return a Feature if features returned from getUnionFeature',
+    inject([MapService], (service: MapService) => {
+      const featureStub = {
+        type: 'Feature',
+        properties: { 'n': 'name' },
+        geometry: { type: 'Polygon', coordinates: [[[1, 1], [2, 2]]] }
+      } as GeoJSON.Feature<GeoJSON.Polygon>;
+      const mapboxStub = {
+        queryRenderedFeatures: (a, b) => [featureStub]
+      };
+      service.setMapInstance(mapboxStub);
+      expect(service.getUnionFeature('layer', featureStub)).toBeTruthy();
+    }
+  ));
 });
