1d58aa91dad4e76084e16be70ca42262da9d3dd5
pjsier <pjsier@gmail.com>
2018-03-06 11:06:44 -0600

    Include data in cloudfront distro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,13 +6,13 @@ export const environment = {
   deployUrl: 'https://beta.evictionlab.org/tool/',
   tileBaseUrl: 'https://tiles.evictionlab.org/',
   evictorsRankingDataUrl: 'https://beta.evictionlab.org/tool/assets/MOCK_EVICTORS.csv',
-  cityRankingDataUrl: 'https://s3.amazonaws.com/eviction-lab-data/rankings/cities-rankings.csv',
-  stateRankingDataUrl: 'https://s3.amazonaws.com/eviction-lab-data/rankings/states-rankings.csv',
-  usAverageDataUrl: 'https://s3.amazonaws.com/eviction-lab-data/avg/us.json',
+  cityRankingDataUrl: 'https://beta.evictionlab.org/data/rankings/cities-rankings.csv',
+  stateRankingDataUrl: 'https://beta.evictionlab.org/data/rankings/states-rankings.csv',
+  usAverageDataUrl: 'https://beta.evictionlab.org/data/avg/us.json',
   mapboxApiKey: 'pk.' +
     'eyJ1IjoiZXZpY3Rpb24tbGFiIiwiYSI6ImNqY20zamVpcTBwb3gzM28yb292YzM3dXoifQ.' +
     'uKgAjsMd4qkJNqEtr3XyPQ',
-  mapboxCountyUrl: 'https://s3.amazonaws.com/eviction-lab-data/search/counties.csv',
+  mapboxCountyUrl: 'https://beta.evictionlab.org/data/search/counties.csv',
   downloadBaseUrl: 'https://exports.evictionlab.org',
   minYear: 2000,
   maxYear: 2016,
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -6,13 +6,13 @@ export const environment = {
     deployUrl: 'https://staging.evictionlab.org/tool/',
     tileBaseUrl: 'https://tiles.evictionlab.org/',
     evictorsRankingDataUrl: 'https://staging.evictionlab.org/tool/assets/MOCK_EVICTORS.csv',
-    cityRankingDataUrl: 'https://s3.amazonaws.com/eviction-lab-data/rankings/cities-rankings.csv',
-    stateRankingDataUrl: 'https://s3.amazonaws.com/eviction-lab-data/rankings/states-rankings.csv',
-    usAverageDataUrl: 'https://s3.amazonaws.com/eviction-lab-data/avg/us.json',
+    cityRankingDataUrl: 'https://staging.evictionlab.org/data/rankings/cities-rankings.csv',
+    stateRankingDataUrl: 'https://staging.evictionlab.org/data/rankings/states-rankings.csv',
+    usAverageDataUrl: 'https://staging.evictionlab.org/data/avg/us.json',
     mapboxApiKey: 'pk.' +
         'eyJ1IjoiZXZpY3Rpb24tbGFiIiwiYSI6ImNqYzJoNzVxdzAwMTMzM255dmsxM2YwZWsifQ.' +
         '4et5d5nstXWM5P0JG67XEQ',
-    mapboxCountyUrl: 'https://s3.amazonaws.com/eviction-lab-data/search/counties.csv',
+    mapboxCountyUrl: 'https://staging.evictionlab.org/data/search/counties.csv',
     downloadBaseUrl: 'https://exports-dev.evictionlab.org',
     minYear: 2000,
     maxYear: 2016,
