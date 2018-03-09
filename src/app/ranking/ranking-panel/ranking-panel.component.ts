faf6871699b0abaa22c0b0eaa08663ea2fc56df6
Lane Olson <lane@onlea.org>
2018-03-09 14:11:54 -0700

    add keyboard nav to rankings

--- src/app/ranking/ranking-panel/ranking-panel.component.ts
+++ src/app/ranking/ranking-panel/ranking-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, Input, Output, EventEmitter, AfterViewInit, ElementRef
+} from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { RankingLocation } from '../ranking-location';
 
@@ -8,7 +10,7 @@ import { RankingLocation } from '../ranking-location';
   styleUrls: ['./ranking-panel.component.scss'],
   providers: [DecimalPipe]
 })
-export class RankingPanelComponent {
+export class RankingPanelComponent implements AfterViewInit {
   @Input() rank: number;
   @Input() topCount: number;
   @Input() location: RankingLocation;
@@ -17,5 +19,21 @@ export class RankingPanelComponent {
   @Output() goToNext = new EventEmitter();
   @Output() close = new EventEmitter();
   @Output() locationClick = new EventEmitter<number>();
+
+  constructor(private el: ElementRef) {}
+
+  /**
+   * Grab focus when the panel opens
+   * NOTE / TODO: Giving focus to the ranking panel will scroll the page to the bottom
+   *    which is not desired behaviour. Alternatively, the user will need to tab through
+   *    the entire list to get focus of an element in the ranking panel.  Commenting
+   *    out the focus until we determine a best approach.
+   */
+  ngAfterViewInit() {
+    // const buttons = this.el.nativeElement.getElementsByTagName('button');
+    // if (buttons.length) {
+    //   buttons[0].focus();
+    // }
+  }
 
 }
