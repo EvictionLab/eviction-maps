a597dd09c2d2c2cb8278d8e7ad342e5aeedf38b9
pjsier <pjsier@gmail.com>
2018-03-13 10:43:47 -0500

    Fixes fetching tile data from search

--- src/app/map-tool/map-tool.service.ts
+++ src/app/map-tool/map-tool.service.ts
@@ -6,6 +6,8 @@ import { TranslateService } from '@ngx-translate/core';
 import * as SphericalMercator from '@mapbox/sphericalmercator';
 import * as vt from '@mapbox/vector-tile';
 import * as Protobuf from 'pbf';
+import inside from '@turf/inside';
+import { point } from '@turf/helpers';
 import * as bbox from '@turf/bbox';
 import 'rxjs/add/observable/forkJoin';
 import * as _isEqual from 'lodash.isequal';
@@ -298,13 +300,11 @@ export class MapToolService {
    * @param lonLat array of [lon, lat]
    * @param multiYear specifies whether to merge multiple year tiles
    */
-  getTileData(
-    geoid: string, lonLat: number[], multiYear = false
-  ): Observable<MapFeature> {
+  getTileData(geoid: string, lonLat: number[], multiYear = false): Observable<MapFeature> {
     const layerId = this.geoidLayerMap[geoid.length];
     const queryZoom = this.getQueryZoom(layerId, lonLat);
     const coords = this.getXYFromLonLat(lonLat, queryZoom);
-    const parseTile = this.getParser(geoid, layerId, lonLat);
+    const parseTile = this.getParser(geoid, layerId, lonLat, queryZoom, coords);
 
     if (multiYear) {
       const tileRequests = this.tilesetYears.map((y) => {
@@ -314,6 +314,23 @@ export class MapToolService {
     } else {
       return this.requestTile(layerId, coords, queryZoom).map(parseTile);
     }
+  }
+
+  /**
+   * Takes a feature returned from search and returns associated tile data
+   * @param feat
+   */
+  getSearchTileData(feat: MapFeature): Observable<MapFeature> {
+    const layerId = feat.properties.layerId as string;
+    const lonLat = feat['center'] as number[];
+    const queryZoom = this.getQueryZoom(layerId, lonLat);
+    const coords = this.getXYFromLonLat(lonLat, queryZoom);
+
+    const parseTile = this.getSearchParser(feat, queryZoom, coords);
+    const tileRequests = this.tilesetYears.map((y) => {
+      return this.requestTile(layerId, coords, queryZoom, y).map(parseTile);
+    });
+    return Observable.forkJoin(tileRequests).map(this.mergeFeatureProperties.bind(this));
   }
 
   /**
@@ -400,10 +417,10 @@ export class MapToolService {
    * @param geoid
    * @param layerId
    * @param lonLat
+   * @param queryZoom
+   * @param coords
    */
-  private getParser(geoid, layerId, lonLat) {
-    const queryZoom = this.getQueryZoom(layerId, lonLat);
-    const coords = this.getXYFromLonLat(lonLat, queryZoom);
+  private getParser(geoid, layerId, lonLat, queryZoom, coords) {
     return (res: ArrayBuffer): MapFeature => {
       const tile = new vt.VectorTile(new Protobuf(res));
       const layer = tile.layers[layerId];
@@ -418,6 +435,68 @@ export class MapToolService {
 
       const matchFeat = features.find(f => f.properties['GEOID'] === geoid);
       const centerFeat = centerFeatures.find(f => f.properties['GEOID'] === geoid);
+
+      if (matchFeat && centerFeat) {
+        matchFeat.properties = { ...matchFeat.properties, ...centerFeat.properties };
+        return this.processMapFeature(matchFeat, layerId);
+      }
+
+      return {} as MapFeature;
+    };
+  }
+
+  /**
+   * Tile parser for MapFeature objects returned from search
+   * @param feat
+   * @param queryZoom
+   * @param coords
+   */
+  private getSearchParser(feat: MapFeature, queryZoom: number, coords: Object) {
+    const layerId = feat.properties.layerId as string;
+    const lonLat = feat['center'] as number[];
+    const pt = point(lonLat);
+
+    return (res: ArrayBuffer): MapFeature => {
+      const tile = new vt.VectorTile(new Protobuf(res));
+      const layer = tile.layers[layerId];
+      const centerLayer = tile.layers[`${layerId}-centers`];
+
+      const features = [...Array(layer.length)].fill(null).map((d, i) => {
+        return layer.feature(i).toGeoJSON(coords['x'], coords['y'], queryZoom);
+      });
+      const centerFeatures = [...Array(centerLayer.length)].fill(null).map((d, i) => {
+        return centerLayer.feature(i);
+      });
+      const containsPoint = features.filter(f => inside(pt, f));
+
+      let matchFeat, centerFeat;
+      // If looking for block groups, search only on geography
+      // otherwise search on name and geography
+      if (layerId === 'block-groups') {
+        if (containsPoint.length > 0) {
+          matchFeat = containsPoint[0];
+        } else if (features.length > 0) {
+          matchFeat = features[0];
+        }
+      } else {
+        const featName = (feat.properties['label'] as string)
+          .replace(',', '').split(' ')[0].toLowerCase();
+        const nameFilter = (f) => f.properties['n'].toLowerCase().startsWith(featName);
+
+        const containsMatchName = containsPoint.filter(nameFilter);
+        const matchesName = features.filter(nameFilter);
+
+        if (containsMatchName.length > 0) {
+          matchFeat = containsMatchName[0];
+        } else if (matchesName.length > 0) {
+          matchFeat = matchesName[0];
+        }
+      }
+
+      if (matchFeat) {
+        centerFeat = centerFeatures.find(f =>
+          f.properties['GEOID'] === matchFeat.properties['GEOID']);
+      }
 
       if (matchFeat && centerFeat) {
         matchFeat.properties = { ...matchFeat.properties, ...centerFeat.properties };
