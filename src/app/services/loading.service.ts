f24cd0e86a17b60f1757b97c6f385dc1bc2e95ff
Lane <laneolson@gmail.com>
2018-03-22 11:16:03 -0600

    logging for loading service and predictive search

--- src/app/services/loading.service.ts
+++ src/app/services/loading.service.ts
@@ -4,13 +4,15 @@ import { Injectable } from '@angular/core';
 export class LoadingService {
   isLoading = false;
   private loadingItems = [];
+  private loadingStore = {};
 
   constructor() { }
 
   /**
    * Triggers loading to start for a given identifier
    */
-  start(id: string) {
+  start(id: string, data?:any) {
+    console.log('loading start', id, data);
     this.isLoading = true;
     if (this.loadingItems.indexOf(id) === -1) {
       this.loadingItems.push(id);
@@ -21,7 +23,8 @@ export class LoadingService {
    * Marks an identifier as done loading, sets loading status to false if
    * no more items are left to load.
    */
-  end(id: string) {
+  end(id: string, data?:any) {
+    console.log('loading end', id, data);
     this.loadingItems = this.loadingItems.filter(itemId => itemId !== id);
     this.isLoading = !(this.loadingItems.length === 0);
   }
