9eb62fa03633d17eb34bb4375d6e90d6860482f3
Lane <laneolson@gmail.com>
2018-03-24 16:15:40 -0600

    update about us links and wording, update footer link hover state

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -21,7 +21,7 @@ export const environment = {
     { url: 'https://beta.evictionlab.org/', langKey: 'NAV.HOME' },
     { url: 'https://beta.evictionlab.org/map', langKey: 'NAV.MAP' },
     { url: 'https://beta.evictionlab.org/rankings', langKey: 'NAV.RANKINGS' },
-    { url: 'https://beta.evictionlab.org/about-us', langKey: 'NAV.ABOUT' },
+    { url: 'https://beta.evictionlab.org/about', langKey: 'NAV.ABOUT' },
     { url: 'https://beta.evictionlab.org/why-eviction-matters', langKey: 'NAV.PROBLEM' },
     { url: 'https://beta.evictionlab.org/methods', langKey: 'NAV.METHODS' },
     { url: 'https://beta.evictionlab.org/help-faq', langKey: 'NAV.HELP' },
